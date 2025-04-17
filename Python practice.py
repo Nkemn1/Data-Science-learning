9d347d689258caf4defaf451e6e5ba6f40e69a71
Nkemdilim Nwafor <Nkemdilimn@gmail.com>
2025-04-17 15:16:34 +0100

    Assignment

---
+++ Python practice.py
@@ -0,0 +1,6 @@
+# 1. Write a program that prints your full name, your favourite hobby, and a one-line 
+# description about yourself. 
+
+print("My name is Nkem Nwafor.")
+print("My favourite hobby is reading and swimming.")
+print("I am an undergraduate student currently dabbling in Data science")
